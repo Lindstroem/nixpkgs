aad98800408e0a21411173a1aed529d1b6c23e5f
Bart Brouns <bart@magnetophon.nl>
2015-03-27 19:51:29 +0100

    add nova-filters: LADSPA plugins based on filters of nova

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11387,6 +11387,8 @@ let
 
   novaclient = callPackage ../applications/virtualization/nova/client.nix { };
 
+  nova-filters =  callPackage ../applications/audio/nova-filters { };
+
   nspluginwrapper = callPackage ../applications/networking/browsers/mozilla-plugins/nspluginwrapper {};
 
   nvi = callPackage ../applications/editors/nvi { };
