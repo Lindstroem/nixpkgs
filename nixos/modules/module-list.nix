7786f3f7a89a505544202c62e06307b014a422eb
Arseniy Seroka <ars.seroka@gmail.com>
2015-04-05 17:19:19 +0300

    impl: teamviewer daemon

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -227,6 +227,7 @@
   ./services/monitoring/smartd.nix
   ./services/monitoring/statsd.nix
   ./services/monitoring/systemhealth.nix
+  ./services/monitoring/teamviewer.nix
   ./services/monitoring/ups.nix
   ./services/monitoring/uptime.nix
   ./services/monitoring/zabbix-agent.nix
